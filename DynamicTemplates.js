a0e0315d184212334d9209a18e7ea84a480db831
Mustafa Yücel <mustafayucel@gmail.com>
2018-10-15 16:24:08 +0300

    Create DynamicTemplates.js

---
+++ DynamicTemplates.js
@@ -0,0 +1,13 @@
+// Fills template strings dynamically
+// * Inspired from the answers at https://stackoverflow.com/questions/30003353/can-es6-template-literals-be-substituted-at-runtime-or-reused
+const fillTemplate = function(templateString, templateVars){
+  var func = new Function(...Object.keys(templateVars),  "return `"+templateString +"`;")
+  return func(...Object.values(templateVars));
+}
+
+// Sample
+var getVal = function() { 
+  var hosting = "overview/id/d:${Id}";
+  var domain = {Id:1234, a:22};
+  return fillTemplate(hosting, domain);
+}
